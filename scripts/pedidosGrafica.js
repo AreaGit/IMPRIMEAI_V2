94ac482aeafd08efbc3fc52be133c19deec920b1
AreaGit <areagit5@gmail.com>
2024-05-13 11:34:37 -0300

    Mostrando pedidos

--- scripts/pedidosGrafica.js
+++ scripts/pedidosGrafica.js
@@ -1,6 +1,27 @@
 fetch('/pedidos-cadastrados')
 .then(response => response.json())
 .then(data => {
-    console.log(data);          
+    console.log(data);
+    const pedidosList = document.getElementById('pedidos-list');
+        data.pedidos.forEach(pedidos => {
+            const li = document.createElement('div');
+            li.id = `pedido${pedidos.idPed}`
+            li.className = `cxPedido`
+            li.style.display = 'block';
+            const dataCriacao = new Date(pedidos.createdAt);
+            const dataFormatada = dataCriacao.toLocaleString('pt-BR');
+            const imgUrl = `/imagens/${pedidos.idProduto}`;
+            li.style.display = 'block';
+            li.innerHTML = `
+                <img src="${imgUrl}"></img>
+                <h2 class="ped-nome">${pedidos.nomeProd}</h2>
+                <p class="ped-id">ID ${pedidos.idPed}</p>
+                <p class="ped-valor">R$ ${pedidos.valorProd}</p>
+                <p class="ped-quant">${pedidos.quantidade} unidades</p>
+                <p id="dataCriacao">${dataFormatada}</p>
+                <a href="detalhes-pedidos.html?idPedido=${pedidos.idPed}&idProduto=${pedidos.idProduto}" id="verPed">Detalhes do Pedido</a>
+            `;
+            pedidosList.appendChild(li);
+        })
 })
 .catch(error => console.error('Erro ao buscar pedidos:', error));
